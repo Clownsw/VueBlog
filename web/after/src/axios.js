e0d7f0e77ee9d8646497b6237617f65645e0583a
Clownsw <msmliexx1@gmail.com>
2022-04-05 15:11:05 +0800

    optimize code

--- web/after/src/axios.js
+++ web/after/src/axios.js
@@ -1,17 +1,15 @@
 import axios from 'axios'
 import {Message} from "element-ui";
 
-axios.defaults.baseURL='http://localhost:9999/'
+axios.defaults.baseURL = 'http://localhost:9999/'
 
 // 后置拦截
 axios.interceptors.response.use(response => {
     return response;
 }, error => {
 
-    let msg = undefined;
     let data = error.response;
-
-    msg = error.response.data.message;
+    let msg = error.response.data.message;
 
     if (data.status === 404 || data.status === 403) {
         Message.error(msg)
