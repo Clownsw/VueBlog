268011eda1b0a01f88de214272e9e8839399c00b
Smliexx <msmliexx1@gmail.com>
2022-08-20 23:13:56 +0800

    finish search blog ui
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- web/before/src/components/BlogSearch.vue
+++ web/before/src/components/BlogSearch.vue
@@ -1,26 +1,98 @@
 <template>
-  <div>
     <el-dialog
-      title="提示"
+      class="searchDialog"
       :visible.sync="searchDialogIsShow"
-      width="30%"
+      width="40%"
     >
+      <div class="searchDialogTitle" slot="title">
+        搜索
+        <el-divider></el-divider>
+      </div>
+      <el-input class="searchDialogInput" v-model="searchStr" @change="searchInputChangeHandler"></el-input>
+      <el-card
+        v-for="item in searchBlog"
+        :key="item.id"
+        style="margin-bottom: 10px"
+      >
+        <h3 class="blog-title">
+          <router-link
+            :to="{ name: 'BlogDetail', params: { blogId: item.id } }"
+          >
+            {{ item.title }}
+          </router-link>
+        </h3>
+
+        <p class="blog-description">{{ item.description }}</p>
+      </el-card>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import searchApi from "@/api/search";
 export default {
   name: "BlogSearch",
   data() {
     return {
       searchDialogIsShow: false,
+      searchStr: "",
+      searchBlog: [],
     };
   },
   methods: {
+    searchInputChangeHandler(newValue) {
+      this.searchStr = newValue;
+      if (newValue === "") {
+        this.searchBlog = [];
+      } else {
+        searchApi.searchBlog(newValue).then((resp) => {
+          this.searchBlog = resp.data.data;
+        });
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
+a {
+  text-decoration: none;
+}
+
+.blog-title ::v-deep a {
+  color: #303133;
+}
+
+.blog-description {
+  color: #a0a3a8;
+}
+
+.blog-footer {
+  font-size: 13px;
+  color: #a0a3a8;
+  margin-block-start: 1em;
+  margin-block-end: 0;
+}
+
+.searchDialog .searchDialogTitle {
+  text-align: left;
+}
+
+.searchDialog .searchDialogInput {
+  margin-bottom: 10px;
+}
+
+.searchDialog ::v-deep .el-dialog__header {
+  padding: 20px 20px 0 !important;
+}
+
+.searchDialog ::v-deep .el-divider--horizontal {
+  margin: 18px 0 !important;
+}
+
+.searchDialog ::v-deep .el-dialog__body {
+  padding-top: 0 !important;
+  padding-bottom: 20px !important;
+  text-align: left !important;
+}
 </style>
