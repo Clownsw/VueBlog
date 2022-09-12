de2eddc2f86932c2695ae1515c203c3675b78b17
Smliexx <msmliexx1@gmail.com>
2022-09-12 17:39:50 +0800

    optimize admin page, blog list limit page component
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- server/vueblog-common/src/pojo/limit.rs
+++ server/vueblog-common/src/pojo/limit.rs
@@ -6,32 +6,37 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Limit<T> {
     pub datas: Vec<T>,
-    pub total: i64,   // 所有个数
-    pub size: i64,    // 每页个数
-    pub current: i64, // 当前页
-    pub pages: i64,   // 总页数
+    pub total: i64,        // 所有个数
+    pub page_size: i64,    // 每页个数
+    pub currnet_page: i64, // 当前页
+    pub pages: i64,        // 总页数
 }
 
 impl<T> Limit<T> {
-    pub fn from_unknown_datas(size: i64, all_count: i64, current: i64, datas: Vec<T>) -> Limit<T> {
+    pub fn from_unknown_datas(
+        page_size: i64,
+        all_count: i64,
+        currnet_page: i64,
+        datas: Vec<T>,
+    ) -> Limit<T> {
         let total: i64 = all_count;
         let mut pages: i64 = 0;
 
         if total > 0 {
-            if total <= size {
+            if total <= page_size {
                 pages = 1;
-            } else if total % size == 0 {
-                pages = total / size;
+            } else if total % page_size == 0 {
+                pages = total / page_size;
             } else {
-                pages = (total / size) + 1;
+                pages = (total / page_size) + 1;
             }
         }
 
         Self {
             datas,
             total,
-            size,
-            current,
+            page_size,
+            currnet_page,
             pages,
         }
     }
