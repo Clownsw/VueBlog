f89200da57cb0499629f5aa0ba3e978757d914d4
Smliexx <msmliexx1@gmail.com>
2023-03-09 13:38:47 +0800

    update cdn url
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- web/before/src/views/BlogDetail.vue
+++ web/before/src/views/BlogDetail.vue
@@ -72,27 +72,27 @@ export default {
       externalLink: {
             markdown_css: function() {
                 // 这是你的markdown css文件路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/markdown/github-markdown.min.css';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/markdown/github-markdown.min.css';
             },
             hljs_js: function() {
                 // 这是你的hljs文件路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/highlightjs/highlight.min.js';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/highlightjs/highlight.min.js';
             },
             hljs_css: function(css) {
                 // 这是你的代码高亮配色文件路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/highlightjs/styles/' + css + '.min.css';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/highlightjs/styles/' + css + '.min.css';
             },
             hljs_lang: function(lang) {
                 // 这是你的代码高亮语言解析路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/highlightjs/languages/' + lang + '.css';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/highlightjs/languages/' + lang + '.css';
             },
             katex_css: function() {
                 // 这是你的katex配色方案路径路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/katex/katex.min.css';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/katex/katex.min.css';
             },
             katex_js: function() {
                 // 这是你的katex.js路径
-                return 'https://cdn.smilex.cn/cdn/mavon-editor/katex/katex.min.js';
+                return 'https://cdn.smilex.vip/cdn/mavon-editor/katex/katex.min.js';
             },
       }
     };
